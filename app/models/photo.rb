6da49d561f2a7b58087960004fe0578cad3d6cfd
Thimios Dimopulos <thimios@wantedpixel.com>
2012-09-13 19:06:53 +0100

    added validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
   attr_accessible :category_id, :description, :title
   belongs_to :category, :touch => true, :inverse_of => :photos
+  validates :category_id, :presence => true
 end
