8275e69435b8be7fa7c11e5a3a1b26bdd255f28e
Thimios Dimopulos <thimios@wantedpixel.com>
2012-10-07 22:28:02 +0200

    Login from the web works again
    Show vote number on photo details view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,39 @@
 class SessionsController < Devise::SessionsController
-#  include Devise::Controllers::InternalHelpers
+  #include Devise::Controllers::Helpers
   prepend_before_filter :require_no_authentication, :only => [:new, :create]
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 
   def new
     super
   end
 
   def create
-    user = warden.authenticate(:scope => :user)
-    if user
-      user.reset_authentication_token!
-      render :json => {:auth_token => user.authentication_token, :token_type => "persistant"}, :callback => params[:callback]
-    else
-      render :json => {:error => "invalid_grant"}, :callback => params[:callback]
+    respond_to do |format|
+      format.html { super}
+      format.json {
+        user = warden.authenticate(:scope => :user)
+        if user
+          user.reset_authentication_token!
+          render :json => {:auth_token => user.authentication_token, :token_type => "persistant"}, :callback => params[:callback]
+        else
+          render :json => {:error => "invalid_grant"}, :callback => params[:callback]
+        end
+
+      }
     end
+
+
   end
 
 end
