5039c7e61c1f4a40b08266468def701108aba4a6
Thimios Dimopulos <thimios@wantedpixel.com>
2012-10-25 00:50:17 +0200

    FIX: unicorn with capistrano

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,2 +1,2 @@
-listen 'unix:#{shared_path}/pids/unicorn.socket'
-pid    'unix:#{shared_path}/pids/unicorn.pid'
+listen '/home/deploy/app/shared/pids/unicorn.socket'
+pid    '/home/deploy/app/shared/pids/unicorn.pid'
