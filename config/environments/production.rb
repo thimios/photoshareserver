75cdae2f8c5c737737ed8b71f476b684161cc056
Thimios Dimopulos <thimios@wantedpixel.com>
2012-10-25 16:30:33 +0200

    ADD: mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,22 @@ TodosSt2::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # TODO: Configure mailer options for the other envs as well
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :sendmail
+
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  # :address              => "smtp.gmail.com",
+  # :port                 => 587,
+  # :domain               => 'baci.lindsaar.net',
+  # :user_name            => '<username>',
+  # :password             => '<password>',
+  # :authentication       => 'plain',
+  # :enable_starttls_auto => true  }
+
 end
