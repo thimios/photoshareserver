0c07ec4f6a0710890b3672d140c7540e44f39c16
Thimios Dimopulos <thimios@wantedpixel.com>
2012-10-25 17:11:42 +0200

    ADD: db tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -115,6 +115,24 @@ namespace :deploy do
   task :setup_solr_data_dir do
     run "mkdir -p #{shared_path}/solr/data"
   end
+
+  task :seed do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:seed"
+  end
+end
+
+namespace :db do
+  task :drop,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:drop"
+  end
+
+  task :setup,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:setup"
+  end
+
+  task :seed,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:seed"
+  end
 end
 
 namespace :solr do
