9adc2110d7817ee845d34b6863ad6c990fa2ca5e
Thimios Dimopulos <thimios@wantedpixel.com>
2012-10-30 16:58:34 +0100

    FIX: updated unicornt config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,2 +1,31 @@
 listen '/home/deploy/app/shared/pids/unicorn.socket'
 pid    '/home/deploy/app/shared/pids/unicorn.pid'
+
+# Should be 'production' by default, otherwise use other env
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+# Log everything to one file
+stderr_path "log/unicorn.log"
+stdout_path "log/unicorn.log"
+
+
+# Set unicorn options
+worker_processes 1
+preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
