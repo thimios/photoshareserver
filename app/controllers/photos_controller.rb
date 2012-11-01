34b3430adc278a117f78d7ade3089704e85990df
Thimios Dimopulos <thimios@wantedpixel.com>
2012-11-01 18:46:52 +0100

    ADD: dont delete photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -166,7 +166,8 @@ class PhotosController < ApplicationController
   # DELETE /photos/1.json
   def destroy
     @photo = Photo.find(params[:id])
-    @photo.destroy
+    logger.debug "no photos can be deleted"
+    #@photo.destroy
     respond_to do |format|
       format.html { redirect_to photos_url }
       format.json { head :no_content }
