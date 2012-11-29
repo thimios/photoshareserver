f73825103ac5124684b289a75508ae245b19e3c4
Thimios Dimopulos <thimios@wantedpixel.com>
2012-11-29 19:49:09 +0100

    FIX: forgot password procedure

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,21 @@
+
+class PasswordsController < Devise::PasswordsController
+  prepend_before_filter :require_no_authentication
+
+
+  # PUT /resource/password
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      flash_message = :updated_not_active
+      set_flash_message(:notice, flash_message) if is_navigational_format?
+
+      respond_with resource, :location => "/"
+    else
+      respond_with resource
+    end
+  end
+
+
+end
