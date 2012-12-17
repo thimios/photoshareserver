98dab96ef3945e9d86e9bc6431f316456dde56db
Thimios Dimopulos <thimios@wantedpixel.com>
2012-12-17 18:01:01 +0200

    FIX: font mimetypes

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+
+#Mime::Type.register "font/opentype", :font
+# tell Rack (and Sprockets) about modern font MIME types:
+Rack::Mime::MIME_TYPES['.woff'] = 'application/x-font-woff'
+Rack::Mime::MIME_TYPES['.ttf'] = 'application/x-font-ttf'
