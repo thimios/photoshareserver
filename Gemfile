cc5b6bb7267cf89829d1300aa67b41fc14887f3b
Thimios Dimopulos <thimios@wantedpixel.com>
2012-12-23 00:59:54 +0200

    FIX: basic functional test running

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.2.3'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-  gem 'sqlite3'
+#  gem 'sqlite3'
 # gem 'pg'
 
 gem 'jquery-rails'
@@ -83,6 +83,7 @@ gem 'unicorn'                 # Use unicorn as the app server
 
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
+
 
 group :development do
   gem 'capistrano'
--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -1,49 +1,19 @@
 require 'test_helper'
+include Devise::TestHelpers
 
 class PhotosControllerTest < ActionController::TestCase
   setup do
+    sign_in User.first
     @photo = photos(:one)
   end
 
   test "should get index" do
+
+
     get :index
     assert_response :success
     assert_not_nil assigns(:photos)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
 
-  test "should create photo" do
-    assert_difference('Photo.count') do
-      post :create, photo: { category_id: @photo.category_id, description: @photo.description, title: @photo.title }
-    end
-
-    assert_redirected_to photo_path(assigns(:photo))
-  end
-
-  test "should show photo" do
-    get :show, id: @photo
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @photo
-    assert_response :success
-  end
-
-  test "should update photo" do
-    put :update, id: @photo, photo: { category_id: @photo.category_id, description: @photo.description, title: @photo.title }
-    assert_redirected_to photo_path(assigns(:photo))
-  end
-
-  test "should destroy photo" do
-    assert_difference('Photo.count', -1) do
-      delete :destroy, id: @photo
-    end
-
-    assert_redirected_to photos_path
-  end
 end
