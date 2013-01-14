557cd242efd9ce95ecfa3230b0bbe6a4ded6f330
Thimios Dimopulos <thimios@wantedpixel.com>
2013-01-14 14:40:01 +0100

    FIX: no authentication necessary to show the quote

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
 
     class QuotesController < ApplicationController
-      before_filter :my_authenticate_user
+      #before_filter :my_authenticate_user no authentication necessary to get the quote
       # the api is always available to all logged in users
       skip_authorization_check
 
