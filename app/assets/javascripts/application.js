e45e8256186f9b35af6b25dc7338ad5a11aae027
Thimios Dimopulos <thimios@wantedpixel.com>
2013-01-17 15:45:04 +0100

    REMOVE: unused javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,20 +14,3 @@
 //= require jquery_ujs
 //= require h5bp
 
-
-
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID.
-    -->
-
-    var _gaq = [
-    ['_setAccount', 'UA-XXXXX-X'],
-    ['_trackPageview']
-    ];
-    (function (d, t) {
-        var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-        g.src = ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g, s)
-        }(document, 'script'));
-
-
