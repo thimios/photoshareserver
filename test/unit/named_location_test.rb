e238f4cc13ea1a158ff46b656f4c120e9ce57f7a
Thimios Dimopulos <thimios@wantedpixel.com>
2013-01-30 19:49:12 +0100

    ADD: test named_location default avatar when no photos

--- test/unit/named_location_test.rb
+++ test/unit/named_location_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class NamedLocationTest < ActiveSupport::TestCase
-  fixtures :categories, :users, :photos, :named_locations
+  fixtures :categories, :users, :photos, :named_locations, :system_photos
 
   test "followed by current user" do
     current_user = User.find(1)
@@ -13,5 +13,14 @@ class NamedLocationTest < ActiveSupport::TestCase
     current_user.follow location
 
     assert_equal "true", location.followed_by_current_user
+  end
+
+  test "named locations without photos, should have default avatar" do
+    location = NamedLocation.find(1)
+    location.photos.all.each{|photo| photo.destroy}
+
+    defaultavatar = SystemPhoto.find_by_title "default avatar"
+    assert_not_nil location.small_size_url
+    assert_equal defaultavatar.thumb_size_url, location.small_size_url, "locations without photos should have the default avatar"
   end
 end
