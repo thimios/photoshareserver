95e557fd28c532643de23a86f5ca45cfd96057eb
Thimios <thimios@wantedpixel.com>
2013-02-07 14:03:16 +0100

    ADD: test

--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "track location string attribute" do
+    imagefile = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'defaultavatar.png'))
+
+    photo = Photo.create(title: Faker::Lorem.sentence(2).truncate(23), category_id: 1, user_id: 1, latitude: 52.2, longitude: 12.3, image: imagefile, track_location: "yes"))
+    assert true, photo.show_on_map
+  end
 end
