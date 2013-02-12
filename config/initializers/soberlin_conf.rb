160b4edf2a540d37ced3d71504472b352a4e411c
Thimios <thimios@wantedpixel.com>
2013-02-12 20:09:26 +0100

    ADD: require solr logging

--- config/initializers/soberlin_conf.rb
+++ config/initializers/soberlin_conf.rb
@@ -1,4 +1,4 @@
-
+require "sunspot/rails/solr_logging"
 #Rails.configuration.banned_original_size_url = bannedphoto.original_size_url
 #Rails.configuration.banned_full_size_url = bannedphoto.full_size_url
 #Rails.configuration.banned_medium_size_url = bannedphoto.medium_size_url
