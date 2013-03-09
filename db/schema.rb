f1e6809baf7b74f0d571bb73ff0d7ceedcc169f1
Thimios <thimios@wantedpixel.com>
2013-03-09 16:31:23 +0100

    Change category names on the web page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218143801) do
+ActiveRecord::Schema.define(:version => 20130309134237) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(:version => 20130218143801) do
   create_table "categories", :force => true do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "display_title"
   end
 
   create_table "comments", :force => true do |t|
