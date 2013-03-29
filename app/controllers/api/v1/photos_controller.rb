35548eaf6bc4de997005f071aa8ef7075add7a48
thimios <thimios@wantedpixel.com>
2013-03-29 15:53:54 +0100

    fix: sort indexbbox photos with created_at desc

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -36,28 +36,8 @@ module Api
             with(:show_on_map, true)
 
             paginate(:page => 1, :per_page => 60)
+            order_by :created_at, :desc
 
-            adjust_solr_params do |solr_params|
-              #Points = (clicks + 1) * exp(c1 * distance) * exp(c2 * time)
-              #
-              #c1 and c2 are negative constants.
-              #Distance is the distance between the current location and the picture
-              #time the time between the current time and the time the picture was taken.
-              #Clicks is the amount of "so berlin" votes the photo has received
-              #
-              #The constants are:
-              #c1 = -7e-4
-              #c2 = -1.15e-09
-              #Assuming distance in km for c1 and milliseconds for c2.
-              solr_params[:sort] = "product( sum(plusminus_i,1),
-                                        exp(
-                                          product(
-                                              -1.15e-09,
-                                              ms(NOW/HOUR, created_at_dt)
-                                          )
-                                        )
-                                     ) desc".gsub(/\s+/, " ").strip
-            end
           end
 
           photos = []
