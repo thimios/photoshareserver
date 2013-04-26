0027d57106c04954139a737543938dee19ca0e77
thimios <thimios@wantedpixel.com>
2013-04-26 03:18:31 +0200

    revert to using linode

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,100 +1,211 @@
-# This is a sample Capistrano config file for rubber
 
-set :rails_env, Rubber.env
 
-on :load do
-  set :application, rubber_env.app_name
-  set :runner,      rubber_env.app_user
-  set :deploy_to,   "/mnt/#{application}-#{Rubber.env}"
-  set :copy_exclude, [".git/*", ".bundle/*", "log/*", ".rvmrc", ".rbenv-version"]
-end
 
-# Use a simple directory tree copy here to make demo easier.
-# You probably want to use your own repository for a real app
-set :scm, "git"
-set :repository, "git@bitbucket.org:thimios/engineyardhipster.git"
-set :deploy_via, :remote_cache
-set :ssh_options, { :forward_agent => true }
+# This capistrano deployment recipe is made to work with the optional
+# StackScript provided to all Rails Rumble teams in their Linode dashboard.
+#
+# After setting up your Linode with the provided StackScript, configuring
+# your Rails app to use your GitHub repository, and copying your deploy
+# key from your server's ~/.ssh/github-deploy-key.pub to your GitHub
+# repository's Admin / Deploy Keys section, you can configure your Rails
+# app to use this deployment recipe by doing the following:
+#
+# run  ssh -T -oStrictHostKeyChecking=no git@bitbucket.org
+# copy public key to bitbucket
+# apt-get install openjdk-7-jdk
+# apt-get install imagemagick
+# apt-get install nginx
+# copy config/deploy/ssl to /etc/nginx/ssl (only keys are needed, see config/deploy/nginx_conf.erb)
+# rm /etc/nginx/sites-enabled/default
+#  apt-get remove apache2
 
-# Easier to do system level config as root - probably should do it through
-# sudo in the future.  We use ssh keys for access, so no passwd needed
-set :user, 'root'
-set :password, nil
+# 1. Add `gem 'capistrano'` to your Gemfile.
+# 2. Run `bundle install --binstubs --path=vendor/bundles`.
+# 3. Run `bin/capify .` in your app's root directory.
+# 4. Replace your new config/deploy.rb with this file's contents.
+# 5. Configure the two parameters in the Configuration section below.
+# 6. Run `git commit -a -m "Configured capistrano deployments."`.
+# 7. Run `git push origin master`.
+# 8. Run `bin/cap deploy:setup`.
+# 9. Run `bin/cap deploy:migrations` or `bin/cap deploy`.
 
-# Use sudo with user rails for cap deploy:[stop|start|restart]
-# This way exposed services (mongrel) aren't running as a privileged user
+# bin/cap deploy:solr_start
+# bin/cap deploy:seed
+#
+# Note: You may also need to add your local system's public key to
+# your GitHub repository's Admin / Deploy Keys area.
+#
+# Note: When deploying, you'll be asked to enter your server's root
+# password. To configure password-less deployments, see below.
+
+#############################################
+##                                         ##
+##              Configuration              ##
+##                                         ##
+#############################################
+
+GIT_REPOSITORY_URL = 'git@bitbucket.org:thimios/engineyardhipster.git'
+LINODE_SERVER_HOSTNAME = 'www.soberlin.org'
+
+
+#############################################
+#############################################
+
+# General Options
+
+set :bundle_flags,               "--deployment"
+
+set :application,                "soberlin"
+set :normalize_asset_timestamps, false
+set :rails_env,                  "production"
+
+set :user,                       "deploy"
+set :deploy_to,                  "/home/#{user}/app"
+set :server_name,                LINODE_SERVER_HOSTNAME
+
+# Password-less Deploys (Optional)
+#
+# 1. Locate your local public SSH key file. (Usually ~/.ssh/id_rsa.pub)
+# 2. Execute the following locally: (You'll need your Linode server's root password.)
+#
+#    cat ~/.ssh/id_rsa.pub | ssh root@LINODE_SERVER_HOSTNAME "cat >> ~/.ssh/authorized_keys"
+#
+# 3. Uncomment the below ssh_options[:keys] line in this file.
+#
+# ssh_options[:keys] = ["~/.ssh/id_rsa"]
+
 set :use_sudo, true
+set :sudo_user, "deploy"
 
-# How many old releases should be kept around when running "cleanup" task
-set :keep_releases, 3
+default_run_options[:pty] = true
 
-# Lets us work with staging instances without having to checkin config files
-# (instance*.yml + rubber*.yml) for a deploy.  This gives us the
-# convenience of not having to checkin files for staging, as well as 
-# the safety of forcing it to be checked in for production.
-set :push_instance_config, Rubber.env != 'production'
+# SCM Options
+set :scm,        :git
+set :repository, GIT_REPOSITORY_URL
+set :branch,     "master"
 
-# don't waste time bundling gems that don't need to be there 
-set :bundle_without, [:development, :test, :staging] if Rubber.env == 'production'
+set :rvm_type, :user  # Copy the exact line. I really mean :user here
+set :normalize_asset_timestamps, false  # Убирает сёр ошибок со старыми папками жаваскрипта и имаджов
 
-# Allow us to do N hosts at a time for all tasks - useful when trying
-# to figure out which host in a large set is down:
-# RUBBER_ENV=production MAX_HOSTS=1 cap invoke COMMAND=hostname
-max_hosts = ENV['MAX_HOSTS'].to_i
-default_run_options[:max_hosts] = max_hosts if max_hosts > 0
+set :rvm_ruby_string, 'ruby-1.9.3-p194@senchatouch2'
+set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 
-# Allows the tasks defined to fail gracefully if there are no hosts for them.
-# Comment out or use "required_task" for default cap behavior of a hard failure
-rubber.allow_optional_tasks(self)
 
-# Wrap tasks in the deploy namespace that have roles so that we can use FILTER
-# with something like a deploy:cold which tries to run deploy:migrate but can't
-# because we filtered out the :db role
+# Roles
+
+server LINODE_SERVER_HOSTNAME, :app, :web, :db, :primary => true
+
+before 'deploy:restart', 'deploy:migrate'
+# Install RVM
+# before 'deploy:setup',   'rvm:install_rvm'
+# Install Ruby
+# before 'deploy:setup',   'rvm:install_ruby'
+# Or create gemset
+# before 'deploy',         'rvm:create_gemset'
+after  'deploy',         'deploy:cleanup'
+
+after "deploy:setup", "nginx:setup", "nginx:reload"
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
+  end
+end
+
+after "deploy", "rvm:trust_rvmrc"
+
 namespace :deploy do
-  rubber.allow_optional_tasks(self)
-  tasks.values.each do |t|
-    if t.options[:roles]
-      task t.name, t.options, &t.body
+  task :setup_solr_data_dir do
+    run "mkdir -p #{shared_path}/solr/data"
+  end
+end
+
+namespace :db do
+  task :drop,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:drop"
+  end
+
+  task :setup,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:setup"
+  end
+
+  task :load_schema,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:schema.load"
+  end
+
+  task :seed,:roles => :db do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:seed"
+  end
+end
+
+namespace :solr do
+  desc "start solr"
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids --solr-home=#{current_path}/solr  --log-file=#{shared_path}/log/sunspot-solr-production.log"
+  end
+  desc "stop solr"
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids --solr-home=#{current_path}/solr --log-file=#{shared_path}/log/sunspot-solr-production.log"
+  end
+
+  desc "reindex the whole database"
+  task :reindex, :roles => :app do
+    stop
+    # run "rm -rf #{shared_path}/solr/data"
+    start
+
+    input = ''
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex[,,true]" do |channel, stream, data|
+      next if data.chomp == input.chomp || data.chomp == ''
+      print data
+      channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
     end
   end
 end
 
-namespace :deploy do
-  namespace :assets do
-    rubber.allow_optional_tasks(self)
-    tasks.values.each do |t|
-      if t.options[:roles]
-        task t.name, t.options, &t.body
-      end
-    end
+task :install_unicorn_init_script, :roles => :app do
+  set :user, sudo_user
+  run "#{sudo} cp #{latest_release}/config/deploy/unicorn /etc/init.d/unicorn.#{application}"
+  run "#{sudo} chmod 755 /etc/init.d/unicorn.#{application}"
+  run "#{sudo} update-rc.d unicorn.#{application} defaults"
+end
+
+task :install_solr_init_script, :roles => :app do
+  set :user, sudo_user
+  run "#{sudo} cp #{latest_release}/config/deploy/solr /etc/init.d/solr.#{application}"
+  run "#{sudo} chmod 755 /etc/init.d/solr.#{application}"
+  run "#{sudo} update-rc.d solr.#{application} defaults"
+end
+
+
+desc "tail production log files"
+task :tail_logs, :roles => :app do
+  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+    puts  # for an extra line break before the host name
+    puts "#{channel[:host]}: #{data}"
+    break if stream == :err
   end
 end
 
-# load in the deploy scripts installed by vulcanize for each rubber module
-Dir["#{File.dirname(__FILE__)}/rubber/deploy-*.rb"].each do |deploy_file|
-  load deploy_file
+desc "remotely console"
+task :console, :roles => :app do
+  input = ''
+  run "cd #{current_path} && ./script/console #{ENV['RAILS_ENV']}" do |channel, stream, data|
+    next if data.chomp == input.chomp || data.chomp == ''
+    print data
+    channel.send_data(input = $stdin.gets) if data =~ /^(>|\?)>/
+  end
 end
 
-# capistrano's deploy:cleanup doesn't play well with FILTER
-after "deploy", "cleanup"
-after "deploy:migrations", "cleanup"
-task :cleanup, :except => { :no_release => true } do
-  count = fetch(:keep_releases, 5).to_i
-  
-  rsudo <<-CMD
-    all=$(ls -x1 #{releases_path} | sort -n);
-    keep=$(ls -x1 #{releases_path} | sort -n | tail -n #{count});
-    remove=$(comm -23 <(echo -e "$all") <(echo -e "$keep"));
-    for r in $remove; do rm -rf #{releases_path}/$r; done;
-  CMD
-end
+after 'deploy:setup', 'deploy:setup_solr_data_dir'
+#after 'deploy:update_code',  'install_unicorn_init_script'
+#after 'deploy:update_code',  'install_solr_init_script'
+after 'unicorn:stop', 'solr:stop'
+before 'inicorn:start', 'solr:start'
 
-if Rubber::Util.has_asset_pipeline?
-  # load asset pipeline tasks, and reorder them to run after
-  # rubber:config so that database.yml/etc has been generated
-  load 'deploy/assets'
-  callbacks[:after].delete_if {|c| c.source == "deploy:assets:precompile"}
-  callbacks[:before].delete_if {|c| c.source == "deploy:assets:symlink"}
-  before "deploy:assets:precompile", "deploy:assets:symlink"
-  after "rubber:config", "deploy:assets:precompile"
-end
+require "rvm/capistrano"
+require "bundler/capistrano"
+require "capistrano-unicorn"
+require 'capistrano/nginx/tasks'
+#require "capistrano-file_db"
+load 'deploy/assets'
