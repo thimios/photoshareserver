eaf572aeb17f7bd9b98994a5d3dc3bf74758cd6d
thimios <thimios@wantedpixel.com>
2013-12-10 22:48:38 +0100

    no cdn for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ TodosSt2::Application.configure do
   config.assets.precompile += %w( *.js *.css )
 
   # put assets on cdn
-  config.action_controller.asset_host = "d2ujlosr6qi8kc.cloudfront.net"
+  # config.action_controller.asset_host = "d2ujlosr6qi8kc.cloudfront.net"
 
   # store assets in a 'folder' instead of bucket root
   config.assets.prefix = "/production/assets"
