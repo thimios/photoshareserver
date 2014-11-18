7c5e13d379faba94eeaaee8fa78555b8f0d163d8
thimios <thimios@wantedpixel.com>
2014-11-18 14:22:32 +0200

    added standard categories fixture

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Category, :type => :model do
+describe Category, :type => :model do
+  fixtures :all
   it "should validate presence of title" do
     expect(subject).to validate_presence_of :title
+  end
+
+  it "should always have three standard categories with predefined ids" do
+    fashion = Category.find_by_title "fashion"
+    place = Category.find_by_title "place"
+    art = Category.find_by_title "art"
+    expect(fashion.id).to be(1)
+    expect(place.id).to be(2)
+    expect(art.id).to be(3)
   end
 end
