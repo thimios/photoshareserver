8e14191fafabf9ba216298863407aa6224b7a1ca
thimios <thimios@wantedpixel.com>
2014-11-19 19:28:14 +0200

    stop using fixtures

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe Category, :type => :model do
-  fixtures :all
+
   it "should validate presence of title" do
     expect(subject).to validate_presence_of :title
   end
 
   it "should always have three standard categories with predefined ids" do
+    create :category, :fashion
+    create :category, :place
+    create :category, :art
     fashion = Category.find_by_title "fashion"
     place = Category.find_by_title "place"
     art = Category.find_by_title "art"
