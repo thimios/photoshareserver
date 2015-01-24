86c36d84d4732e8d34e49a0b5fdb1ffd0af44337
thimios <thimios@wantedpixel.com>
2015-01-24 20:06:01 +0200

    do not use class attributes when local values can do

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -14,9 +14,9 @@ module Api
 
         unless params[:user_id].nil?
           user_whose_history = params[:user_id] == current_user.id ? current_user : User.find(params[:user_id])
-          @activities = PublicActivity::Activity.where("(recipient_id IN (?) OR owner_id = ?) AND activities.key IN ('photo.create', 'vote.create', 'comment.create')",user_whose_history.photo_ids, user_whose_history.id).order("created_at DESC").page(params[:page]).per(params[:limit])
-          @histories = Array.new
-          @activities.each do |activity|
+          activities = PublicActivity::Activity.where("(recipient_id IN (?) OR owner_id = ?) AND activities.key IN ('photo.create', 'vote.create', 'comment.create')",user_whose_history.photo_ids, user_whose_history.id).order("created_at DESC").page(params[:page]).per(params[:limit])
+          histories = Array.new
+          activities.each do |activity|
 
             #:created_at, #date, time string
             #:title, #Posted a photo, Commented on Jimmy's photo, Liked Jimmy's photo
@@ -26,71 +26,71 @@ module Api
             #:thumb_url, # the url of the thumbnail of the photo posted, liked or commented
 
 
-            @history = History.new
-            @history.created_at = activity.created_at
-            @history.created_at_date = activity.created_at.strftime("%d %b. %Y")
-            @history.actor_id= activity.owner_id
+            history = History.new
+            history.created_at = activity.created_at
+            history.created_at_date = activity.created_at.strftime("%d %b. %Y")
+            history.actor_id= activity.owner_id
             actor = User.find(activity.owner_id)
             actor.current_user= current_user
-            @history.actor_followed_by_current_user= actor.followed_by_current_user
+            history.actor_followed_by_current_user= actor.followed_by_current_user
             if activity.key == "comment.create"
-              @comment = Comment.find(activity.trackable_id)
-              @photo = Photo.find(@comment.commentable_id)
+              comment = Comment.find(activity.trackable_id)
+              @photo = Photo.find(comment.commentable_id)
               if current_user.id == @photo.user_id
-                @history.title = "#{actor.username} commented on your photo."
+                history.title = "#{actor.username} commented on your photo."
               else
-                @history.title = "#{actor.username} commented on #{@photo.author_name}'s photo."
+                history.title = "#{actor.username} commented on #{photo.author_name}'s photo."
               end
 
-              @history.description = @comment.body
-              @history.photo_id = @photo.id
-              @history.comment_id = @comment.id
-              @history.thumb_url = @photo.thumb_size_url
+              history.description = comment.body
+              history.photo_id = @photo.id
+              history.comment_id = comment.id
+              history.thumb_url = @photo.thumb_size_url
 
             elsif activity.key == "photo.create"
-              @photo = Photo.find(activity.trackable_id)
-              @history.title = "Posted a photo."
+              photo = Photo.find(activity.trackable_id)
+              history.title = "Posted a photo."
 
-              @history.description = @photo.title
-              @history.photo_id = @photo.id
-              @history.comment_id = ""
-              @history.thumb_url = @photo.thumb_size_url
+              history.description = photo.title
+              history.photo_id = photo.id
+              history.comment_id = ""
+              history.thumb_url = photo.thumb_size_url
 
             elsif activity.key == "photo.destroy"
-              @photo = Photo.find(activity.trackable_id)
-              @history.title = "#{actor.username} deleted a photo."
+              photo = Photo.find(activity.trackable_id)
+              history.title = "#{actor.username} deleted a photo."
 
-              @history.description = @photo.title
-              @history.photo_id = @photo.id
-              @history.comment_id = ""
-              @history.thumb_url = @photo.thumb_size_url
+              history.description = photo.title
+              history.photo_id = photo.id
+              history.comment_id = ""
+              history.thumb_url = photo.thumb_size_url
 
             elsif activity.key == "vote.create"
-              @vote = Vote.find(activity.trackable_id)
-              @photo = Photo.find(@vote.voteable_id)
+              vote = Vote.find(activity.trackable_id)
+              photo = Photo.find(vote.voteable_id)
 
-              if current_user.id == @photo.user_id
-                @history.title = "#{actor.username} liked your photo."
+              if current_user.id == photo.user_id
+                history.title = "#{actor.username} liked your photo."
               else
-                @history.title = "#{actor.username} liked #{@photo.author_name}'s photo."
+                history.title = "#{actor.username} liked #{photo.author_name}'s photo."
               end
 
-              @history.description = @photo.title
-              @history.photo_id = @photo.id
-              @history.comment_id = ""
-              @history.thumb_url = @photo.thumb_size_url
+              history.description = photo.title
+              history.photo_id = photo.id
+              history.comment_id = ""
+              history.thumb_url = photo.thumb_size_url
             else
               logger.debug "Should not load activity with key: #{activity.key} "
             end
 
-            @histories << @history
+            histories << history
           end
         else
           # TODO: respond with error
         end
 
 
-        render :json =>  { :records => @histories, :total_count => @activities.total_count }
+        render :json =>  { :records => histories, :total_count => activities.total_count }
 
         end
 
